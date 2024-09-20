3680ce8ec4690a1f6ae317492fba0b0fd408968d
Panthevm <panthevm@yandex.ru>
2024-09-20 12:25:20 +0300

    Add Storybook dark mode class target

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import { themes } from '@storybook/theming';
 
 export const parameters = {
   darkMode: {
+    classTarget: 'body',
     stylePreview: true,
     dark:  {...themes.dark, appPreviewBg: 'transparent'},
     light: {...themes.light, appBg: 'white'}
